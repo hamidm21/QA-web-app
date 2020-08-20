a8967af642524f2fddadd3b27d4f67eb299009a0
hamid moradi <hordimad21@gmail.com>
2020-08-20 10:24:11 +0430

    add new page

---
+++ pages/how.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="home mt-16">
+    <div class="flex flex-col w-full justify-center items-center">
+      <div class="flex justify-center items-center w-full mt-2">
+        <p class="text-xl font-bold">چگونه کار میکند ؟</p>
+        <img class src="~/assets/icons/full-logo.png" style="width: 80px; height: 50px;" />
+      </div>
+      <p class="text-center mt-2">.حل یاب در حل سوالات و انجام پروژه ها به شما کمک می کند</p>
+    </div>
+    <hr />
+    <div class="flex justify-center">
+      <div class="w-3/4 flex flex-col mt-5">
+        <div class="flex flex-col sm:flex-row justify-between items-center w-full my-10">
+          <div class="flex sm:w-1/2 justify-center items-center">
+            <img src="~/assets/icons/full-logo.png" />
+          </div>
+          <div class="sm:w-1/2">
+            <h6
+              style="direction: rtl;"
+              class="flex justify-center items-center text-center text-xl"
+              href="#"
+            >
+              <span
+                class="flex text-white text-sm font-bold justify-center items-center mx-1 bg-red w-6 h-6 rounded-full"
+              >1</span>در حل یاب ثبت
+              نام کنید .
+            </h6>
+            <p style="direction: rtl;" class="text-center">
+              اگر قبلا ثبت نام کرده اید به اکانت خود وارد شوید
+              <br />در این مرحله با وارد کردن شماره تلفن همراه خود و رمز عبور ،
+              <br />در حل یاب ثبت نام کنید
+            </p>
+          </div>
+        </div>
+
+        <div class="flex flex-col-reverse sm:flex-row justify-between items-center w-full my-10">
+          <div class="sm:w-1/2">
+            <h6
+              style="direction: rtl;"
+              class="flex justify-center items-center text-center text-xl"
+              href="#"
+            >
+              <span
+                class="flex text-white text-sm font-bold justify-center items-center mx-1 bg-red w-6 h-6 rounded-full"
+              >2</span>پروژه خود را
+              قرار دهید .
+            </h6>
+            <p style="direction: rtl;" class="text-center w-75 ml-auto mr-auto">
+              عنوان و جزئیات پروژه ی خود را در فیلد های مشخص شده قرار دهید
+              <br />در این مرحله ، نوع پروژه ، عنوان پروژه ، توضیحات مربوط به پروژه قیمت
+              پیشنهادی ، پیوست کردن فایل های مربوطه ، و اطلاعات تکمیلی (مقطع
+              تحصیلی ، رشته تحصیلی ، عنوان درس و تاریخ پایان پروژه) را در فیلدهای
+              مشخص شده وارد کنید و پروژه ی خود را ثبت کنید
+              <br />همچنین پس از ثبت پروژه می توانید پروژه ی خود را ویرایش کنید یا آن را
+              لغو کنید
+            </p>
+          </div>
+          <div class="flex sm:w-1/2 justify-center">
+            <img src="~/assets/icons/full-logo.png" />
+          </div>
+        </div>
+    
+        <div class="flex flex-col sm:flex-row justify-between items-center w-full my-10">
+          <div class="flex sm:w-1/2 justify-center">
+            <img src="~/assets/icons/full-logo.png" />
+          </div>
+          <div class="sm:w-1/2">
+            <h6
+              style="direction: rtl;"
+              class="flex justify-center items-center text-center text-xl"
+              href="#"
+            >
+              <span
+                class="flex text-white text-sm font-bold justify-center items-center mx-1 bg-red w-6 h-6 rounded-full"
+              >3</span>بهترین
+              پیشنهاد ممکن را انتخاب کنید .
+            </h6>
+            <p style="direction: rtl;" class="text-center w-75 ml-auto mr-auto">
+              پس از انتخاب پیشنهاد مورد نظر خود ، می توانید هزینه ی آن را از طریق
+              کیف پول حل یاب یا درگاه اینترنتی پرداخت کنید
+              <br />در این مرحله با وارد کردن شماره تلفن همراه خود و رمز عبور ،
+              <br />پس از پرداخت هزینه ی انجام پروژه ، کارشناسی که برای انجام پروژه
+              پیشنهاد داده است ، در زمان معین شده ، پروژه ی شما را انجام می دهد و
+              پاسخ خود را در قسمت پاسخ ها دریافت می کنید
+            </p>
+          </div>
+        </div>
+
+        <div class="flex flex-col-reverse sm:flex-row justify-between items-center w-full my-10">
+          <div class="sm:w-1/2">
+            <h6
+              style="direction: rtl;"
+              class="flex justify-center items-center text-center text-xl"
+              href="#"
+            >
+              <span
+                class="flex text-white text-sm font-bold justify-center items-center mx-1 bg-red w-6 h-6 rounded-full"
+              >4</span>پروژه ی
+              خود را پایان دهید.
+            </h6>
+            <p style="direction: rtl;" class="text-center w-75 ml-auto mr-auto">
+              پروژه ی خود را پس از دریافت جواب پایان دهید
+              <br />در این مرحله ، پس از اینکه حل سوال یا پروژه ی خود را از استادی که
+              پیشنهاد او را قبول کرده اید ، دریافت کردید ، بر روی دکمه ی تایید
+              حل پرسش کلیک کردید ، می توانید به پاسخ دریافتی خود امتیاز دهید و
+              پاسخ دریافتی را تایید کرده و به پروژه ی خود پایان دهید
+              <br />همچنین پس از ثبت پروژه می توانید پروژه ی خود را ویرایش کنید یا آن
+              را لغو کنید
+            </p>
+          </div>
+          <div class="flex sm:w-1/2 justify-center">
+            <img src="~/assets/icons/full-logo.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <p class="flex justify-center items-center text-center sm:text-xl mt-10 mb-5">.هم اکنون ، پروژه ی خود را در حل یاب قرار دهید</p>
+    <div class="text-center">
+      <button type="button" class="bg-green rounded-md p-2 shadow-md mb-4 text-white hover:shadow-xl">+ ثبت پروژه</button>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "~/components/landing/footer.vue";
+
+export default {
+  components: {
+    Footer,
+  },
+  auth: false,
+  layout: "landing",
+};
+</script>
