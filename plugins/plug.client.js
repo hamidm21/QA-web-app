5c577f50ec50e4c65ce80652c9a77015e4cd3a42
Mohsen  Rahmi <mm1377rr@gmail.com>
2020-11-20 17:23:06 +0000

    Add new file

---
+++ plugins/plug.client.js
@@ -0,0 +1,11 @@
+export default function() {
+  !(function(e, t, a) {
+    "use strict";
+    var s = t.head || t.getElementsByTagName("head")[0],
+      p = t.createElement("script");
+    (e.iwmfBadge = a),
+      (p.async = true),
+      (p.src = "https://cdn.iwmf.ir/js/people-votes/people-vote-1-1.js"),
+      s.appendChild(p);
+  })(window, document, "o-top-right");
+}
