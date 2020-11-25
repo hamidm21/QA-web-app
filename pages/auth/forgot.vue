c801e09cb6a3c9aafa99782b49f1aabf30a6a58d
Mohsen  Rahmi <mm1377rr@gmail.com>
2020-11-25 08:44:09 +0000

    Update forgot.vue

--- pages/auth/forgot.vue
+++ pages/auth/forgot.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="activation w-full sm:w-auto">
         <div class="sm:w-auto w-full">
-        <form @keydown.enter="forgotSend()" class="bg-white shadow-lg rounded-lg flex w-max-md justify-center m-4 border">
+        <form @keydown.enter="forgotSend()" style="backgroundColor:#fff;" class=" shadow-lg rounded-lg flex w-max-md justify-center m-4 border">
             <div class="px-8 pt-6 mb-4">
               <div>
                 <nuxt-link to="/">
@@ -12,26 +12,26 @@
                 <img src="~/assets/img/halyab_icon.png" alt="icon" class="w-10" style="border-radius: 50%;">
               </div>
               <div class="flex items-center justify-center py-8">
-                <h1 class="font-bold text-size-20">
+                <h1 class="font-bold text-black text-size-20">
                   بازیابی رمز عبور
                 </h1>
               </div>
-                <small>
+                <small class="text-gray-700">
                     رمز جدید به شماره شما ارسال می شود
                 </small>
               <div class="my-4">
               <label class="flex text-gray-700 text-sm font-bold mb-2" for="row">
                   شماره تماس 
               </label>
-              <input v-model="forgot.cell_phone" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="activation" type="number" placeholder="شماره شما" required/>
+              <input v-model="forgot.cell_phone" class="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id="activation" type="number" placeholder="شماره شما" required/>
               </div>
               <div class="flex items-center justify-center">
-              <button @click="forgotSend()" type="button" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline" style="box-shadow: 0 4px 14px 0 rgba(0,118,255,0.39);">
+              <button @click="forgotSend()" type="button" class="text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline" style="backgroundColor:#132A3E;">
                   ارسال
               </button>
               </div>
             </div>
-            <div class="hidden bg-blue-600 text-bold items-center justify-center md:flex md:flex-col md:max-w-md" style="box-shadow: 0 4px 14px 0 rgba(0,118,255,0.39); color:white; border-top-left-radius: inherit; border-bottom-left-radius: inherit;">
+            <div class="hidden  text-bold items-center justify-center md:flex md:flex-col md:max-w-md" style="box-shadow: 0 4px 14px 0 rgba(0,118,255,0.39); color:white; border-top-left-radius: inherit; border-bottom-left-radius: inherit;backgroundColor:#132A3E;">
               <img class="p-4" src="~/assets/img/register.svg" style="width: 50%;" alt="auth-logo">
               <div class="block items-center justify-center pb-2">
                 <h1 class="font-bold" style="font-size: xx-large;">
